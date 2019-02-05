825e9f3b1bfd6123fc25c05589e8ff644b50d7f6
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-05 17:47:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -137,6 +137,8 @@ def adding_to_dennis
      }
      new_language = "Assembly"
 
-programmer_hash[:dennis_ritchie][:languages] = new_language
+programmer_hash[:dennis_ritchie][:languages] << new_language
+
+programmer_hash
 
 end
